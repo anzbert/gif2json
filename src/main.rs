0d82c7b2dda5992c901544cfed5b6e3e2a29ecb7
anzbert <anzbert@gmail.com>
2021-10-25 00:24:38 +0800

    basic functionality works

--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,30 @@ use image::{AnimationDecoder, Pixel};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
-struct Output {
+struct OutputObject {
     frames: Vec<DecodedFrame>,
 }
-impl Output {
+impl OutputObject {
     fn new() -> Self {
         Self { frames: Vec::new() }
     }
     fn add(&mut self, next_frame: DecodedFrame) {
         self.frames.push(next_frame);
     }
+    fn to_json_string(&self) -> String {
+        serde_json::to_string(self).expect("Error creating JSON string")
+    }
 }
 
 #[derive(Serialize, Deserialize)]
 struct DecodedFrame {
-    pixels: Vec<Vec<u8>>,
-    size_x: u32,
-    size_y: u32,
+    dimensions: (u32, u32),
+    pixels: Vec<(u8, u8, u8)>,
 }
 
 impl DecodedFrame {
-    fn new(pixels: Vec<Vec<u8>>, size_x: u32, size_y: u32) -> Self {
-        Self {
-            pixels,
-            size_x,
-            size_y,
-        }
+    fn new(pixels: Vec<(u8, u8, u8)>, dimensions: (u32, u32)) -> Self {
+        Self { dimensions, pixels }
     }
 }
 
@@ -44,7 +42,7 @@ fn main() {
         _ => {
             println!(
                 "\nError: Invalid Parameters\n\n
-            Syntax: img2json [image_file].gif\n"
+            Syntax: gif2json [image_file].gif\n"
             );
             process::exit(1);
         }
@@ -52,10 +50,18 @@ fn main() {
 
     let filename = args.get(1).expect("Error reading first Argument");
 
+    // let data = match std::fs::metadata(filename) {
+    //     Ok(metadata) => metadata,
+    //     Err(err) => {
+    //         println!("Error Reading File's Metadata: {}", err);
+    //         process::exit(1);
+    //     }
+    // };
+
     if !filename.ends_with(".gif") {
         println!(
             "\nError: Invalid Filetype. Not a 'gif'\n\n
-    Syntax: img2json [image_file].gif\n"
+    Syntax: gif2json [image_file].gif\n"
         );
         process::exit(1);
     }
@@ -69,43 +75,39 @@ fn main() {
     };
 
     let decoder = match GifDecoder::new(file) {
-        Ok(decoded) => decoded,
+        Ok(gif) => gif,
         Err(err) => {
             println!("Error decoding Image: {}", err);
             process::exit(1);
         }
     };
 
-    let frames = decoder.into_frames();
-    let frames = frames
+    let frames = decoder
+        .into_frames()
         .collect_frames()
         .expect("Error splitting gif into frames");
 
-    let mut output = Output::new();
+    let mut output = OutputObject::new();
 
     for frame in frames.iter() {
         let image_buffer = frame.buffer();
 
-        let (size_x, size_y) = image_buffer.dimensions();
-
-        let pixels_as_rgb_vec: Vec<Vec<u8>> = image_buffer
+        let pixels_as_rgb_vec: Vec<(u8, u8, u8)> = image_buffer
             .pixels()
             .map(|p| {
-                let (r, g, b, _a) = p.channels4();
-                vec![r, g, b]
+                let (r, g, b, _) = p.channels4(); // ditch alpha
+                (r, g, b)
             })
             .collect();
 
-        let decoded_frame = DecodedFrame::new(pixels_as_rgb_vec, size_x, size_y);
+        let decoded_frame = DecodedFrame::new(pixels_as_rgb_vec, image_buffer.dimensions());
 
         output.add(decoded_frame);
     }
 
-    let output_json = serde_json::to_string(&output).expect("Error creating JSON string");
-
     let mut json_file = File::create("output.json").expect("Error creating output file");
     json_file
-        .write_all(output_json.as_bytes())
+        .write_all(output.to_json_string().as_bytes())
         .expect("Error writing to output file");
     println!("\nWritten to: 'output.json'\n");
 }
